65cfa6269ffc8ec4a43e31c352e3b32f64b44996
SqRoots <lixuan.xyz@gmail.com>
2025-08-24 14:39:23 +0800

    添加了2个绘制MDE图像的函数

--- src/abetter/__init__.py
+++ src/abetter/__init__.py
@@ -1,6 +1,6 @@
 from .ab import AB
 from .sample_mean import SampleMean
 from .sample_prop import SampleProp
-from .io.plot import plot_two_mean, plot_diff_mean
+from .io.plot import plot_two_mean, plot_diff_mean, plot_mde_mean_sample, plot_mde_prop_sample
 
-__all__ = ['AB', 'SampleMean', 'SampleProp', 'plot_two_mean', 'plot_diff_mean']
+__all__ = ['AB', 'SampleMean', 'SampleProp', 'plot_two_mean', 'plot_diff_mean', 'plot_mde_mean_sample', 'plot_mde_prop_sample']
